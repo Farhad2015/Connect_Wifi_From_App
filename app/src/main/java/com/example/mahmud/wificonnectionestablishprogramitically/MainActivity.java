36d620aba4ee5927558d4b2602bd0977e3c111dd
mahmudurG5 <mahmudur@netg5.com>
2017-10-14 11:49:44 +0600

    First Commit

---
+++ app/src/main/java/com/example/mahmud/wificonnectionestablishprogramitically/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.mahmud.wificonnectionestablishprogramitically;
+
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonConnect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonConnect = (Button) findViewById(R.id.btnConnect);
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String networkSSID = "NET-G5 LIMITED";
+                String networkPass = "netgen55";
+
+                WifiConfiguration wifiConfig = new WifiConfiguration();
+                wifiConfig.SSID = String.format("\"%s\"", networkSSID);
+                wifiConfig.preSharedKey = String.format("\"%s\"", networkPass);
+
+
+                WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+                //remember id
+                int netId = wifiManager.addNetwork(wifiConfig);
+                wifiManager.disconnect();
+                wifiManager.enableNetwork(netId, true);
+                wifiManager.reconnect();
+            }
+        });
+    }
+}
